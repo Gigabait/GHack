4f707c70284663b1f3707ca6e8c27cefe3d00fb7
Flex <LUModder@gmail.com>
2015-10-01 12:30:42 -0700

    UI

---
+++ lua/ghack/client/ghack_ui.lua
@@ -0,0 +1,11 @@
+local GHack = {
+  Icons = {
+    Home     = "icon16/home.png",
+    Tasks    = "icon16/.png",
+    Software = "icon16/folder.png",
+    Internet = "icon16/world.png",
+    Logs     = "icon16/book.png",
+    Hardware = "icon16/server.png",
+    Research = "",
+    
+  }
