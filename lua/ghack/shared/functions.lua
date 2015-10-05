05caac09860b0eabcf298d5cfd0ece8d7bf94453
Flex <LUModder@gmail.com>
2015-10-05 12:19:33 -0700

    Misc Functions

---
+++ lua/ghack/shared/functions.lua
@@ -0,0 +1,13 @@
+--Generate random password--
+GHack = GHack or {}
+
+local chars = string.Explode("","abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_$")
+
+function GHack.RandomPassword()
+    local genpw = {}
+    for i = 1,8 do
+        table.insert(genpw,table.Random(chars))
+    end
+    genpw = string.Implode("",genpw)
+    return genpw
+end
